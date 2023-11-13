0581c95de574b0e37be8156d9a7813e6081056f6
sanchar127 <msan53366@gmail.com>
2023-11-13 11:01:26 +0545

    editing color button

--- app/product/[productid]/ProductDetails.tsx
+++ app/product/[productid]/ProductDetails.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import SetColor from "@/app/components/products/SetColor";
 import { Rating } from "@mui/material";
-
+import { useCallback, useState } from "react";
 interface ProductDetailsProps {
   product: any;
 }
@@ -24,9 +25,29 @@ const Horizontal = () => {
   return <hr className="w-[30%] m-2" />;
 };
 const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
+  const [cartProduct, setCartProduct] = useState<CartProductType>({
+    id: product.id,
+
+    name: product.name,
+    description: product.description,
+    category: product.category,
+    brand: product.brand,
+    selectedImg: { ...product.images[0] },
+    quantity: 1,
+    price: product.price,
+  });
+  console.log(cartProduct);
   const productRating =
     product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     product.reviews.length;
+  const handleColorSelect = useCallback(
+    (value: SelectedImgType) => {
+      setCartProduct((prev) => {
+        return { ...prev, selectedImg: value };
+      });
+    },
+    [cartProduct.selectedImg]
+  );
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
       <div>Images</div>
@@ -51,7 +72,11 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
           {product.inStock ? "In stock" : "Out of stock"}
         </div>
         <Horizontal />
-        <div>color</div>
+        <SetColor
+          cartProduct={cartProduct}
+          images={product.images}
+          handleColorSelect={handleColorSelect}
+        />
         <Horizontal />
         <div>Quantity</div>
         <Horizontal />
--- utils/formatPrice.jsx
+++ utils/formatPrice.jsx
@@ -1,8 +1,6 @@
-// utils/formatPrice.ts
-
-export const formatPrice = (amount: number): string => {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(amount);
-};
+// export const formatPrice = (amount: number): string => {
+//     return new Intl.NumberFormat("en-US", {
+//       style: "currency",
+//       currency: "USD",
+//     }).format(amount);
+//   };
